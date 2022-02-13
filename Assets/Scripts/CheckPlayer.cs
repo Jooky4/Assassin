94868092418508652342a73dbb91d8781346a230
Solo <costudiolabor@gmail.com>
2022-02-14 00:49:49 +0300

    Добавил Движение игрока. Начал логику ботов.

---
+++ Assets/Scripts/CheckPlayer.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPlayer : MonoBehaviour
+{
+    [Header("Командир отряда")]
+    [SerializeField]
+    private EnemyController teamLead;
+
+    [SerializeField]
+    private EnemyController enemyController;
+
+    [SerializeField]
+    private MoveController moveController;
+
+    [SerializeField]
+    private Transform visuallDistanceAttack;
+
+    [SerializeField]
+    private float valueScaleDistanceAttack;
+
+    [SerializeField]
+    private float distance;
+
+    [SerializeField]
+    public bool isPlayer;
+
+
+    void Start()
+    {
+        // moveController = GetComponent<MoveController>();
+        valueScaleDistanceAttack = visuallDistanceAttack.localScale.x / 2;
+    }
+
+
+    void Update()
+    {
+        if (!isPlayer) UpdateCheckTarget();
+
+    }
+
+
+    void UpdateCheckTarget()
+    {
+        GetTeamLead();
+        distance = CheckDistance(transform, moveController.transform);
+
+        if (distance < valueScaleDistanceAttack)
+        {
+            isPlayer = true;
+            SetEnemyTarget();
+        }
+    }
+
+
+    public float CheckDistance(Transform firstObj, Transform secondObj)
+    {
+        float result = 0.0f;
+        result = Vector3.Distance(firstObj.position, secondObj.position);
+
+        return result;
+    }
+
+    void SetEnemyTarget()
+    {
+        enemyController.currentTarget = moveController.transform;
+    }
+
+    void GetTeamLead()
+    {
+        if (teamLead)
+        {
+            if (teamLead.currentTarget)
+            {
+                enemyController.currentTarget = teamLead.currentTarget;
+                isPlayer = true;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,361 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum StateGame
+{
+    Start,
+    Game,
+    WinGame,
+    LoseGame
+}
+
+/// <summary>
+/// Game Controller логика игры,содержит ссылки на игрока, шарики и противников
+/// </summary>
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance;
+
+    [HideInInspector]
+    public int countDeadBots;
+
+    [Header("МАХ время игры")]
+    public int maxTimeGame = 10;
+
+    [Header("Мах число жизней замка")]
+    public int maxHealthCastle = 10;
+
+    [Header("Мах число бабла за игру")]
+    public int maxMoneyGame = 100;
+
+    [HideInInspector]
+    public int currentTimerGame;
+
+    [Header("Ссыль SpawnerBots")]
+    [SerializeField]
+    public Transform spawnerBots;
+
+
+    [Header("Ссыль SpawnerBullets")]
+    [SerializeField]
+    public Transform spawnerBullets;
+
+    [Header("Кол-во Spawn снарядов в начале игры")]
+    [SerializeField]
+    public int countBulletsStartGame;
+
+    [Header("Ссыль на панель Старт Игра")]
+    [SerializeField]
+    public GameObject PanelStartGame;
+
+    [Header("Ссыль на панель результата")]
+    [SerializeField]
+    public GameObject PanelResult;
+
+    [Header("Ссыль на панель Победы игрока")]
+    [SerializeField]
+    private GameObject PanelWinGame;
+
+    [Header("Ссыль на панель Поражения игрока")]
+    [SerializeField]
+    private GameObject PanelLoseGame;
+
+    [Header("Задержка на появление панель Win/Lose")]
+    [SerializeField]
+    private float delayOnPanelWinLose = 1.0f;
+
+    [SerializeField]
+    [Header("Мах кол-во ботов")]
+    private int maxCountBots = 10;
+
+    [HideInInspector]
+    public int currentLevel;   //   текущий уровень 
+
+    [HideInInspector]
+    public bool isPlayGame;
+
+    [HideInInspector]
+    public StateGame stateGame;
+
+    [HideInInspector]
+    public int currentHealthCastle; // текущее здоровье замка
+
+    [HideInInspector]
+    public int currentCountBots;   //  текущее кол-во ботов
+
+    [HideInInspector]
+    public int currentMoney;   //  текущее кол-во бабла
+
+    [HideInInspector]
+    public int allMoney;      // всего денях
+
+    [HideInInspector]
+    public int currentIDSkin;   // текущий ID skin
+
+    [HideInInspector]
+    public int currentIDWeapon;   // текущий ID weapon
+
+    [HideInInspector]
+    public int upgradeDobleShootLevel;     //  // текущий Level DoubleShoot
+
+    [HideInInspector]
+    public int currentPriceDoubleShoot;    // цена прокачки DoubleShoot
+
+    [HideInInspector]
+    public int upgradeSpeedHeroLevel;      //  // текущий Level SpeedHero
+
+    [HideInInspector]
+    public int currentPriceSpeedHero;     // цена прокачки SpeedHero
+
+
+    void Awake()
+    {
+        Instance = this;
+        PanelResult.SetActive(false);
+        PanelWinGame.SetActive(false);
+        PanelLoseGame.SetActive(false);
+        spawnerBullets.gameObject.SetActive(false);
+        spawnerBots.gameObject.SetActive(false);
+        PanelStartGame.gameObject.SetActive(true);
+
+        currentLevel = LoadData("LevelNumber");
+
+        if (currentLevel == 0)
+        {
+            currentLevel = 1;
+        }
+
+        allMoney = LoadData("Money");
+        currentIDSkin = LoadData("SavedIDSkin");
+        currentIDWeapon = LoadData("SavedIDWeapon");
+        SetLoadScene(currentLevel);
+    }
+
+
+    void Start()
+    {
+        currentHealthCastle = maxHealthCastle;
+        currentCountBots = maxCountBots;
+        isPlayGame = true;
+        stateGame = StateGame.Start;
+        currentTimerGame = maxTimeGame;
+        StartCoroutine(TimerGame());
+    }
+
+    private void Update()
+    {
+        if (isPlayGame)
+        {
+            UpdateGame();
+            UpdateStateGame();
+        }
+    }
+
+    void UpdateGame()
+    {
+        if (currentHealthCastle <= 0)
+        {
+            stateGame = StateGame.LoseGame;
+        }
+
+        if (currentTimerGame <= 0)
+
+        //if (currentCountBots <= 0 && currentTimerGame <= 0)
+        {
+            stateGame = StateGame.WinGame;
+        }
+    }
+
+    /// <summary>
+    /// обновления игры
+    /// </summary>
+    public void UpdateStateGame()
+    {
+
+        if (stateGame == StateGame.LoseGame)
+        {
+            isPlayGame = false;
+            EndGame();
+            Invoke("LoseGame", delayOnPanelWinLose);
+        }
+
+        if (stateGame == StateGame.WinGame)
+        {
+            isPlayGame = false;
+            EndGame();
+            Invoke("WinGame", delayOnPanelWinLose);
+        }
+    }
+
+    /// <summary>
+    /// старт игры
+    /// </summary>
+    public void StartGame()
+    {
+        stateGame = StateGame.Game;
+        spawnerBullets.gameObject.SetActive(true);
+        spawnerBots.gameObject.SetActive(true);
+        PanelResult.SetActive(true);
+    }
+
+    /// <summary>
+    /// конец игры
+    /// </summary>
+    public void EndGame()
+    {
+        spawnerBullets.gameObject.SetActive(false);
+        spawnerBots.gameObject.SetActive(false);
+        PanelResult.SetActive(false);
+        SaveData("LevelNumber", currentLevel);                            // сохраняем номер level
+
+        int saveMoney = currentMoney + allMoney;
+        SaveData("Money", saveMoney);                                 // сохраняем колво денег
+        StartCoroutine(ShowAnimationMoney());
+        Debug.Log("End Game");
+    }
+
+
+    /// <summary>
+    /// Победа
+    /// </summary>
+    void WinGame()
+    {
+        currentLevel++;
+        currentMoney += maxMoneyGame;
+
+        //maxHealthCastle = SetRandomHealth(currentLevel);
+        //SaveData("MaxHealthCastle", maxHealthCastle);                            // сохраняем мах колво жизней на уровне
+
+        EndGame();
+        Debug.Log("Win Game");
+
+        PanelWinGame.SetActive(true);
+    }
+
+
+    /// <summary>
+    /// Проигрыш
+    /// </summary>
+    void LoseGame()
+    {
+        EndGame();
+
+        Debug.Log("Lose Game");
+        PanelLoseGame.SetActive(true);
+    }
+
+    private IEnumerator ShowAnimationMoney()
+    {
+        print("ShowAnimationMoney");
+        while (currentMoney > 0)
+        {
+            currentMoney--;
+            allMoney++;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+
+
+    /// <summary>
+    /// Таймер игры
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator TimerGame()
+    {
+        while (currentTimerGame > 0)
+        {
+            yield return new WaitForSeconds(1.0f);
+
+            if (stateGame == StateGame.Game)
+            {
+                currentTimerGame--;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Выбор сцены для загрузки
+    /// </summary>
+    /// <param name="currentLevel"></param>
+    /// <returns></returns>
+    int SetLoadScene(int currentLevel)
+    {
+        int result = 0;
+
+        if (currentLevel > 20)
+        {
+            LoadScene(Random.Range(0, 21));
+        }
+        else
+        {
+            LoadScene(currentLevel);
+        }
+        return result;
+    }
+
+    //if (currentScene != SceneManager.GetActiveScene().buildIndex)
+    //{
+    //    SceneManager.LoadScene(currentScene);
+    //}
+
+    public void LoadScene(int currentScene)
+    {
+       
+
+        int tempLoadScene = currentScene - 1;
+
+        print(" currentScene " + currentScene);
+        print(" currentLevel " + currentLevel);
+        print(" SceneManager.sceneCountInBuildSettings " + (SceneManager.sceneCountInBuildSettings - 2));
+        print(" tempLoadScene " + tempLoadScene);
+
+        if (SceneManager.GetActiveScene().buildIndex != tempLoadScene)
+        {
+
+            if ((SceneManager.sceneCountInBuildSettings - 2) >= tempLoadScene)
+            {
+                tempLoadScene = currentScene - 1;
+
+                SceneManager.LoadScene(tempLoadScene);
+            }
+            else
+            {
+                return;
+                //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);                          // текущая сцена
+            }
+        }
+        else
+        {
+            return;
+
+        }
+    }
+
+
+
+
+
+    /// <summary>
+    /// Сохранение данных
+    /// </summary>
+    /// <param name="KeyName"></param>
+    /// <param name="Value"></param>
+    public void SaveData(string KeyName, int Value)
+    {
+        PlayerPrefs.SetInt(KeyName, Value);
+    }
+
+    /// <summary>
+    /// Загрузка данных
+    /// </summary>
+    /// <param name="KeyName"></param>
+    /// <returns></returns>
+    public int LoadData(string KeyName)
+    {
+        return PlayerPrefs.GetInt(KeyName);
+    }
+}
